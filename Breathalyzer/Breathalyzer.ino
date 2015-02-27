8064c1c46aac571ce41ccd0b51dcd9cd5f5277be
Nathen <naorton@gmail.com>
2015-02-26 21:09:22 -0600

    added status colors to indicate breathalyzer status

--- Breathalyzer/Breathalyzer.ino
+++ Breathalyzer/Breathalyzer.ino
@@ -2,9 +2,16 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+int RED_PIN = 6;
+int BLUE_PIN = 10;
+int GREEN_PIN = 9;
+
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
   
   
 }
@@ -21,10 +28,28 @@ This function generates a percentage
           1024
 
 */
+
 float adc_to_volts(int adc_counts) {
   float volts = 0;
   volts = ( (float) adc_counts / 1024.0 ) * 5.0 ;
   return(volts);
+}
+
+void statusColors(int val) {
+
+  if(val<100){
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(RED_PIN,LOW);
+    digitalWrite(BLUE_PIN, HIGH);
+  } else if(val <200){
+    digitalWrite(GREEN_PIN, HIGH);
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(BLUE_PIN,LOW);
+  } else{
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(GREEN_PIN,LOW);
+    digitalWrite(BLUE_PIN,LOW);
+  }
 }
 
 void loop() {
@@ -36,6 +61,7 @@ void loop() {
     lcd.clear();
     val = analogRead(0);
     voltage = adc_to_volts(val);
+    statusColors(val);
      
     lcd.print(val);
     lcd.setCursor(7,0);
@@ -44,7 +70,7 @@ void loop() {
      
   if(val>200) {
     lcd.setCursor(0,1);
-    lcd.print("you're drunk");
+    lcd.print("You're drunk");
      
   }
      
